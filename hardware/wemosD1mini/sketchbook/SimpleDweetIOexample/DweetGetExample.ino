6fce5c9c9b09c482836ce740de7221dd5f658df1
Michael Gries <github@gries.name>
2016-06-05 23:25:16 +0200

    Add files via upload

---
+++ hardware/wemosD1mini/sketchbook/SimpleDweetIOexample/DweetGetExample.ino
@@ -0,0 +1,29 @@
+/*
+ * M. Gries, 2016-06-05
+ * 
+ * Github source used: https://github.com/gamo256/dweet-esp/
+ * 
+ * modified Libraries: dweetESP8266.cpp
+ * 
+ */
+
+#include "dweetESP8266.h"
+#define THIG_NAME  "af104-test"  // Put here your thing name
+#define key  "date_Time"  // Put here your requested key
+#define WIFISSID "HP LaserJet P1505n"
+#define PASSWORD "vQ1tASKOUu"
+
+dweet client;
+
+void setup() {
+    Serial.begin(74880);
+    delay(10);
+    client.wifiConnection(WIFISSID, PASSWORD);
+}
+
+void loop() {
+    // specifies the args of type "String"
+    String value = client.getDweet(THIG_NAME, key);
+    Serial.print(key); Serial.print("=");
+    Serial.println(value);
+}
