d96c6eb3c42616b4269c0e9c8d4c5f28e2556cbf
Michael Gries <github@gries.name>
2016-06-12 23:17:43 +0200

    Add files via upload

---
+++ WDG/Day.js
@@ -0,0 +1,188 @@
+﻿/*
+Libray:   Michael Gries (c)2009-2015
+Creation: 2009-APR-06 (VRC Monitor)
+Modified: 2016-JUN-12 (WDG Network Monitoring)
+Version:  16.6.11
+Notes:    uses SSI functionality; 
+          therefore file extention requires: *.shtm
+*/
+
+function drawLimit(limit, color, boolTrue) {
+var x;
+var y = D.ScreenY(limit); 
+    if (boolTrue) {
+        for (x = left - 5; x < right; x++) {
+            new Pixel(x, y, color);
+        } // horizontal line
+    }
+}
+
+function drawGraph() {
+    var xPixelSize = 1440; // must be xSize in script VRCday.shtm
+    var yStatusValueLine = 100.0 // see s1[1][xxxx] parameter
+   
+       
+    // Parameter N[1]
+    var nSourceFull;
+    nSourceFull = N[1][1];  // vrc_YYYY-MM-DD.log (0-18 characters)
+        nSource = nSourceFull.substring(4,18);
+
+    // Parameter S[1]
+    sBetriebsart = B[1][1];
+        if (sBetriebsart == "00") {
+            sBetriebsart = "Sommerbetrieb"
+        }
+        if (sBetriebsart == "01") {
+            sBetriebsart = "Winterbetrieb"
+        }
+
+    // Parameter C[1]
+    sBDauer = C[1][1]; // totals of heating period in seconds
+
+    // Parameter C[2]
+    sBStart = C[2][1]; // number of heating periods 
+
+    // Parameter T[1]
+    var xPixel = 0;
+    for (var element in T[1]) {
+        if (++xPixel <= xPixelSize) {
+            tAussen = T[1][element];
+            new Pixel(D.ScreenX(xPixel/xFactor), D.ScreenY(tAussen), cAussen);
+        }
+    }
+        
+    // Parameter T[2]
+    var xPixel = 0;
+    for (var element in T[2]) {
+        if (++xPixel <= xPixelSize) {
+            tWasser = T[2][element];
+            new Pixel(D.ScreenX(xPixel/xFactor), D.ScreenY(tWasser), cWasser);
+        }
+    }
+
+    // Parameter T[3]
+    var xPixel = 0;
+    for (var element in T[3]) {
+        if (++xPixel <= xPixelSize) {
+            tKessel = T[3][element];
+            new Pixel(D.ScreenX(xPixel/xFactor), D.ScreenY(tKessel), cKessel);
+        }
+    }
+
+    // Parameter T[4]
+    var xPixel = 0;
+    for (var element in T[4]) {
+        if (++xPixel <= xPixelSize) {
+            tHKurve = T[4][element];
+            new Pixel(D.ScreenX(xPixel/xFactor), D.ScreenY(tHKurve), cHKurve);
+        }
+    }
+    
+    // Parameter S[1]
+    // attention (source: http://www.w3schools.com/js/js_numbers.asp): 
+    // 'Never write a number with a leading zero (like 07)'
+    // 'Some JavaScript versions interpret numbers as octal if they are written with a leading zero'
+    // In this application leading zero is used by sending device (python script)
+    // i.e: S[1][1000]="0256" == 174
+    var xPixel = 0;
+    bService = false;  // last data record indicates Service Bar to be displayed
+    for (var element in S[1]) {
+        if (++xPixel <= xPixelSize) {
+            sBetrieb = S[1][element];
+            sBetriebsstatus = "t.b.d."; // default
+            switch (sBetrieb) {
+            case 0:    
+                sBetriebsstatus = "Bereitschaft (kein Heizbetrieb)";
+                bService = false; // no service (reset indication)
+                break; 
+            case 1:
+                sBetriebsstatus = "Heizbetrieb";
+                newPixelBlock(xPixel, yStatusValueLine, cKessel);
+                bService = false; // no service (reset indication)
+                break;
+            case 174:  
+            case 256:  // due to leading 0 equals octet 174 (compare IE developer tool F12)
+                sBetriebsstatus = "Wartung";
+                new Pixel(D.ScreenX(xPixel/xFactor), D.ScreenY(yStatusValueLine+1), cService);
+                bService = true;
+                break;
+            case 175:
+            case 257:  // equal octet 175 (compare IE developer tool F12)
+                sBetriebsstatus = "Heizbetrieb &amp; Wartung";
+                newPixelBlock(xPixel, yStatusValueLine, cKessel);
+                bService = true;
+                break;
+            case 1024:
+                sBetriebsstatus = "Warmwasser (Anforderung)";
+                newPixelBlock(xPixel, yStatusValueLine, cKessel);
+                bService = false; // no service (reset indication)
+                break;
+            case 1026:
+                sBetriebsstatus = "Warmwasser";
+                newPixelBlock(xPixel, yStatusValueLine, cWasser);
+                bService = false; // no service (reset indication)
+                break;
+            case 1280:  // Speicherladung (Warmwasser) + Wartung
+                sBetriebsstatus = "Speicherladung &amp; Wartung";
+                new Pixel(D.ScreenX(xPixel/xFactor), D.ScreenY(yStatusValueLine+1), cService);
+                // in between no Pixel (i.e. frame line)
+                new Pixel(D.ScreenX(xPixel/xFactor), D.ScreenY(yStatusValueLine-1), cWasser);
+                bService = true;
+                break;
+            case 1282:  // Warmwasser + Pumpe + Wartung
+                sBetriebsstatus = "Warmwasser";
+                newPixelBlock(xPixel, yStatusValueLine, cWasser);
+                bService = true;
+                break;
+            case 1538:
+                sBetriebsstatus = "Warmwasser + ???";
+                newPixelBlock(xPixel, yStatusValueLine, cWasser);
+                bService = false; // no service (reset indication)
+                break;
+            }
+        }
+    }
+    // data record evaluation and displaying ends here
+    // evaluate Service request
+    if (bService) // service request detected
+    {
+        newBarService();  // dynamically display service bar
+    }
+}
+
+function  newPixelBlock(x, y, color) {
+    new Pixel(D.ScreenX(x/xFactor), D.ScreenY(y-1), color);
+    new Pixel(D.ScreenX(x/xFactor), D.ScreenY(y), color);
+    new Pixel(D.ScreenX(x/xFactor), D.ScreenY(y+1), color);
+}
+
+function newBarService(){
+    // Bar coordinates hard-coded
+    // Bar: Service (Wartung)
+    var bar1Text = "Service";
+    var bar1Titel = "Systemmeldung Wartung";
+    new Bar(60+1440-150, 120+240+30, 60+1440, 120+240+30+20, cService, bar1Text, cRED, bar1Titel);
+}
+
+function fuehrendeNull(wert)
+{
+  if (wert<10) return "0" + parseInt(wert);
+  else return parseInt(wert);
+}
+
+function SekundenumwandelnHMS(Sekundenzahl)   // Stunden, Minuten, Sekunden
+{
+ Sekundenzahl = Math.abs(Sekundenzahl)
+ return fuehrendeNull((Sekundenzahl/60/60)%24) + ":" +
+        fuehrendeNull((Sekundenzahl/60)%60) + ":" + fuehrendeNull(Sekundenzahl%60);
+}
+function Sekundenumwandeln(Sekundenzahl)   // Tage, Stunden, Minuten, Sekunden
+{
+ Sekundenzahl = Math.abs(Sekundenzahl)
+ return parseInt(Sekundenzahl/60/60/24)+ " Tage " + fuehrendeNull((Sekundenzahl/60/60)%24) + ":" +
+        fuehrendeNull((Sekundenzahl/60)%60) + ":" + fuehrendeNull(Sekundenzahl%60);
+}
+         
+function nextDiagram() {
+    window.location.href = "http://www.gries.name/VRC/VRCday.shtm";
+}
---
+++ WDG/DayDiagram.js
@@ -0,0 +1,120 @@
+﻿/*
+Libray:   Michael Gries (c)2009-2015
+Creation: 2009-APR-06  (VRC Monitor)
+Modified: 2016-JUN-12 (WDG Network Monitoring)
+Version:  16.6.11
+*/
+
+var now = new Date();
+var dayOfMonth = now.getDate();
+    dayOfMonth = ((dayOfMonth<10) ? "0"+dayOfMonth : dayOfMonth);
+var month = 1 + now.getMonth();
+    month = ((month<10) ? "0" + month : month);
+var monthAndDay = new String();
+    monthAndDay = month + "/" + dayOfMonth; // "mm/dd"
+var year = now.getFullYear();     
+var level = new String();
+var nSource = new String();
+var tAussen = new String(); var cAussen = "#00FF00"; //green
+var tWasser = new String(); var cWasser = "#0000FF"; //blue
+var tKessel = new String(); var cKessel = "#FF0000"; //red
+var tHKurve = new String(); var cHKurve = "#663300"; //brown
+var sBDauer = new String(); var cBDauer = "#808080"; //dark grey
+var sBStart = new String(); var cBStart = "#808080"; //dark grey
+                             var cLimit = "#808080"; //dark grey
+                           var cService = "#FFCC00"; //orange
+
+var sBetrieb = new String();
+var sBetriebsstatus = new String();
+var sBetriebsart = new String();
+var cBLACK = "#000000"; var cWHITE = "#FFFFFF"; var cGREY = "#808080"; var cRED = "#FF0000";
+
+var D = new Diagram();
+var left = 60; var top = 120;
+var xLow = 0; var xHigh = 24; var xFactor = 60;
+var sizeX = (xHigh-xLow) * xFactor; // number of minutes per day
+var yLow = -20; var yHigh = 100; var yFactor = 2;
+var sizeY = (yHigh-yLow) * yFactor; // = no of Pixel
+var right = left + sizeX;
+var bottom = top + sizeY; // Pixel=4000mv/20=200
+D.SetFrame(60, 120, 60+1440, 120+240);
+D.SetBorder(0, 24, -20, 100);
+D.XGridDelta = 1; D.YGridDelta = 10;
+D.XScale = 1; D.YScale = 1; // 1=scale numeric
+D.SetText("<b>[h]</b>", "<b>[°C]</b>", "<B>Betriebsdaten Heizung - Temperaturen[°C] / Heizkurve / Betriebstatus</B>");
+var boolShowDim = true;
+var drawTooltip = "show next diagram"; var drawAction = "nextDiagram()";
+D.Draw("#DEDEDE", "#000000", boolShowDim, drawTooltip, drawAction); // bgd-color: lightgrey
+
+function buildDayDiagram()   // build diagram contents
+{
+    document.open();
+    var legTop = bottom + 30;
+    var legSize = 20;
+    // set legend (Bars) first
+    // Bar: Referenztemperatur
+    var bar0Text = "Referenz: 0 °C";
+    var bar0Titel = "0 Grad Marke";
+    new Bar(60+901, 120+240+30, 60+1440, 120+240+30+20, cGREY, bar0Text, cBLACK, bar0Titel);
+    // now data records should be evaluated and displayed
+    // drawLimit() should be used before drawGraph() to allow that 0 Degree values will not be overwritten
+    drawLimit(0, cLimit, true); // value in °C; draw Limit first (to allow pixel visible on line)
+    drawGraph();
+    // next Bars needs to be called after evaluation of data records in function drawGraph()
+    // Bar: Aussentemperatur
+    var bar1Text = "Aussen: " + tAussen + " °C";
+    var bar1Titel = "Aussentemperaturfühler";
+    new Bar(60, 120+240+30, 60+150, 120+240+30+20, cAussen, bar1Text, cBLACK, bar1Titel);
+    // Bar: Wassertemperatur
+    var bar1Text = "Wasser: " + tWasser + " °C";
+    var bar1Titel = "Temperatur Warmwasserspeicher";
+    new Bar(60+151, 120+240+30, 60+300, 120+240+30+20, cWasser, bar1Text, cWHITE, bar1Titel);
+    // Bar: Kesseltemperatur
+    var bar1Text = "Kessel: " + tKessel + " °C";
+    var bar1Titel = "Temperatur Brennerkessel";
+    new Bar(60+301, 120+240+30, 60+450, 120+240+30+20, cKessel, bar1Text, cWHITE, bar1Titel);
+    // Bar: Heizkurventemp.
+    var bar1Text = "H-Kurve: " + tHKurve + " °C";
+    var bar1Titel = "Witterungsgef&uuml;hrte Vorlauftemperatur";
+    new Bar(60+451, 120+240+30, 60+600, 120+240+30+20, cHKurve, bar1Text, cWHITE, bar1Titel);
+    // Bar: Brennerdauer
+    var iBDauerStunden = SekundenumwandelnHMS(parseInt(sBDauer));
+    var bar1Text = "Dauer: " + iBDauerStunden;
+    var bar1Titel = "Brennerdauer " + sBDauer + " s";
+    new Bar(60+601, 120+240+30, 60+750, 120+240+30+20, cBDauer, bar1Text, cWHITE, bar1Titel);
+    // Bar: Brennerstarts
+    var bar1Text = "Starts: " + sBStart;
+    var bar1Titel = sBStart + " Brennerstarts erfasst";
+    new Bar(60+751, 120+240+30, 60+900, 120+240+30+20, cBStart, bar1Text, cWHITE, bar1Titel);
+    //
+    // new line of legend
+    //
+    // Bar: Betriebszustand (letzter Datensatz)
+    var barTitel = "letzter dargestellter Betriebszustand";
+    new Bar(60+000, 120+240+30+21, 60+300, 120+240+30+21+20, cBLACK, "Status: " + sBetriebsstatus, cWHITE, barTitel);
+    // Bar: Dateiname (Datenquelle))
+    var barTitel = "Rohdatendatei";
+    new Bar(60+301, 120+240+30+21, 60+450, 120+240+30+21+20, cBLACK, nSource, cWHITE, barTitel);
+    // Bar: Betriebsart
+    var barTitel = "Betriebsart (Sommer / Winter)";
+    new Bar(60+451, 120+240+30+21, 60+600, 120+240+30+21+20, cBLACK, sBetriebsart, cWHITE, barTitel);
+    // Bar: Tagesverbrauch
+    var fDurchsatz = 1.91; // kg/h   Düse 60°ST Danfoss
+    var fDichteOel = 0.83; // kg/l   Heizoel EL nach DIN 51603
+    var fLiterProH = fDurchsatz / fDichteOel; // Durchsatz in l/h
+    var fBDauer = parseFloat(sBDauer);
+    var fVerbrauch = fBDauer / 3600 * fLiterProH;
+    var sVerbrauch = "Verbrauch: " + fVerbrauch.toFixed(1) + " l";
+    var barTitel = "Tagesverbrauch bei Düsendurchsatz: " + fLiterProH.toFixed(2) + " (l/h)";
+    new Bar(60+601, 120+240+30+21, 60+750, 120+240+30+21+20, cBLACK, sVerbrauch, cWHITE, barTitel);
+    // Bar: Heizwert
+    var fLeistungProLiter = 9.8; // KW/l   Heizwert Heizoel EL
+    var fEnergie = fVerbrauch * fLeistungProLiter;
+    var sEnergie = "Energie: " + fEnergie.toFixed(0) + " KWh";
+    var barTitel = "errechnete Energieverbrauch nach Brennerstunden bzw. Litern";
+    new Bar(60+751, 120+240+30+21, 60+900, 120+240+30+21+20, cBLACK, sEnergie, cWHITE, barTitel);
+    // Bar: aktuelle Uhrzeit des Clients
+    var barTitel = "aktuelle Uhrzeit";
+    new Bar(60+901, 120+240+30+21, 60+1440,120+240+30+21+20, cBLACK, now, cWHITE, barTitel);
+    document.close();
+}
