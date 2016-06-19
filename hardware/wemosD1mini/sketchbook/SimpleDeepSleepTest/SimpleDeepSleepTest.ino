b2631bcd970056127667e0f298d8c956431f091e
Michael Gries <github@gries.name>
2016-06-19 23:03:32 +0200

    Add files via upload

--- hardware/wemosD1mini/sketchbook/SimpleDeepSleepTest/SimpleDeepSleepTest.ino
+++ hardware/wemosD1mini/sketchbook/SimpleDeepSleepTest/SimpleDeepSleepTest.ino
@@ -5,8 +5,9 @@
  *  (c) 2016, Michael Gries
  *  Creation: 2016-06-15 (based on Sparkfun tutorial ESP8266_Phant_Sleep)
  *  Modified: 2016-06-17 (BUITIN_LED support added to indicate Active Mode)
+ *  Modified: 2016-06-19 (editorial changes)
  * 
- * PREREQUISITE (for Deep-Sleep Wake mode):
+ * PREREQUISITES (hardware dependencies for Deep-Sleep Wake mode):
  *   for Wemos D1 mini: connect pin RST with D0 (GPIO16)
  *   for NodeMCU: shortcut both pads of the not mounted resitor R3
  *   (otherwise if GPIO16 can't trigger RST than ESP8266 will respond with ets_main.c)
@@ -14,23 +15,26 @@
  * LINKS:
  *   see https://github.com/griemide/NodeMCU/tree/master/hardware/wemosD1mini/sketchbook/SimpleDeepSleep
  * 
+ * ISSUES:
+ *   see: https://github.com/esp8266/Arduino/issues/1363
+ *   
  */
 
 #include <ESP8266WiFi.h>
 
 // DECLARATIONS
-const char* ssid     = "HP LaserJet P1505n";
-const char* password = "vQ1tASKOUu";
-const int SerialBaudrate   = 74880; // Wemos D1 mini default
-const int brightnessLED    = 512;   // 0..1023 (0=full)
-const int sleepTimeSeconds = 15;    // * 1E6 
+const char* ssid                = "HP LaserJet P1505n";
+const char* password            = "vQ1tASKOUu";
+const int   SERIAL_BAUDRATE     = 74880; // Wemos D1 mini default
+const int   LED_BRIGHTNESS      = 512;   // 0..1023 (0=full)
+const int   SLEEP_TIME_SECONDS  = 15;    // * 1E6 (internal µs)
 
 void setup() {
-  Serial.begin(SerialBaudrate);  delay(10);
+  Serial.begin(SERIAL_BAUDRATE);  delay(10);
   pinMode(BUILTIN_LED, OUTPUT);  // initialize onboard LED 
-  analogWrite(BUILTIN_LED, brightnessLED);
+  analogWrite(BUILTIN_LED, LED_BRIGHTNESS);
   Serial.println();
-  Serial.print(__func__); Serial.print(": enter setup after ");
+  Serial.print(__func__); Serial.print(": reset by ");
   Serial.println(ESP.getResetReason()); //Deep-Sleep Wake OR  External System
   
   // Connect to WiFi network
@@ -44,9 +48,10 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   // activate Deep-Sleep Wake mode
-  Serial.print(__func__); Serial.print(": activating Deep-Sleep Wake mode for ");
-  Serial.print(sleepTimeSeconds); Serial.println(" seconds");
-  ESP.deepSleep(sleepTimeSeconds * 1E6);
+  Serial.print(__func__); Serial.print(": "); Serial.print(millis()); 
+  Serial.print(" ms -> activating Deep-Sleep Wake mode for ");
+  Serial.print(SLEEP_TIME_SECONDS); Serial.println(" seconds");
+  ESP.deepSleep(SLEEP_TIME_SECONDS * 1E6);
 }
 
 void loop() 
