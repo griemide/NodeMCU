b84b2e75783ac0374d7808c5ba764a3589265469
Michael Gries <github@gries.name>
2016-10-25 00:11:34 +0200

    Add files via upload

--- hardware/wemosD1mini/sketchbook/af104-pws/WLAN.ino
+++ hardware/wemosD1mini/sketchbook/af104-pws/WLAN.ino
@@ -31,7 +31,7 @@ void GetMACaddress()
 {
   byte mac[6];
   WiFi.macAddress(mac);
-  sprintf(deviceMACaddress, "%2x:%2x:%2x:%2x:%2x:%2x",mac[0],mac[1],mac[2],mac[3],mac[4],mac[5]);
+  sprintf(deviceMACaddress, "%02x:%02x:%02x:%02x:%02x:%02x",mac[0],mac[1],mac[2],mac[3],mac[4],mac[5]);
 }
  
 void ConnectingToWLAN()
